2d5e521dd10bc525d7ee3883288e78d5f1fe9f3a
jiayao <jiayao115>
2020-11-01 23:56:54 +0800

    first1101

---
+++ models_ner/albert.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Config(object):
+    def __init__(self):
+        module_name = 'ALBert'
+        save_path = f'../save/{module_name}.pkl'
+
+        batch_size = 128
+        embed = 768
+        layer_num = 6
+        padding_idx = 0
+        max_seq_length = 200
+        vocab_size = 0
+        pass
+
+
+class Module(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.embedding = nn.Embedding(config.vocab_size,
+                                      config.embed,
+                                      config.padding_idx)
+        self.encoder = Encoder()
+
+    def forward(self, input):
+        return
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return
+
+
+class Attention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return
+
+
+class FeedForward(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return
+
+
---
+++ run_ner.py
@@ -0,0 +1,33 @@
+from models_ner import albert
+import argparse
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+import numpy as np
+
+################ args
+parse = argparse.ArgumentParser()
+parse.add_argument('--model', default='albert')
+parse.add_argument('--training', default=True, type=bool)
+parse.add_argument('--data_path', default='./data')
+args = parse.parse_args()
+print(args)
+
+
+################ data
+config = albert.Config()
+dataset_train = []
+dataset_test = []
+dataset_val = []
+dataLoader_train = DataLoader(dataset=dataset_train, batch=config.batch_size)
+dataLoader_test = DataLoader(dataset=dataset_test, batch=config.batch_size)
+dataLoader_val = DataLoader(dataset=dataset_val, batch=config.batch_size)
+print(len(dataLoader_train))
+print(len(dataLoader_test))
+print(len(dataLoader_val))
+
+################ model
+
+model = albert.Module(config)
+
+
+if __name__ == '__main__':
+    pass
